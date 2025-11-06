00e3dbb09ae7198ee17f5d177f5868144a35f07c
rashmikaspal29 <rashmikaspal15@gmail.com>
2025-11-05 20:08:02 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-print("Hello")
+def verse(thing, verb, noise):
+    print(f"The {thing} on the bus {verb} {noise},\n{noise};")
+    print(f"{noise}.\nThe {thing} on the bus {verb} {noise},")
+    print("all through the town.")
+
+def main():
+    verse('wheels', 'go', 'round and round')
+    print()
+    verse('wipers', 'go', 'swish, swish, swish')
+    print()
+    verse('horn', 'goes', 'beep, beep, beep')
+main()
