ee6ef7c2b03d55ae0b08b615e6d311ef7dbb56aa
rashmikaspal29 <rashmikaspal15@gmail.com>
2025-11-20 12:01:48 -0600

    exam practices

--- codingExam.py/art_maker.py
+++ codingExam.py/art_maker.py
@@ -1,5 +1,5 @@
 """
- Name
+ Name Rashmi Kaspal, date = 11/19/2025
  CSC 201
  Programming Project 2
 
@@ -24,6 +24,54 @@ def main():
 #    filenameWithPath = 'gallery/fall_retangular_scene.gif' # use this code if the file chooser doesn't work
 
     #add your code here
-    if not filenameWithPath endwith
+    if not filenameWithPath.endswith('.gif'):
+        print("Invalid file format. Must choose a 'gif' file. ")
+        print("Ending execution.")
+        exit(-1)
+
+
+    myImage = Image(Point(0,0), filenameWithPath)
+    changeFile = filenameWithPath.split('.')
+    artFile = changeFile[0] + '.art'
+    fout = open(artFile, 'w')
+
+    colorNum = int(input("Color for background? (1 white, 2 black) "))
+    
+    if colorNum == 1:
+        color = 'white'
+    elif colorNum == 2:
+        color = 'black'
+    else:
+        color = 'white'
+        colorNum = 1
+        print("Invalid color choice. Using white")
+
+    width = myImage.getWidth()
+    height = myImage.getHeight()
+
+    fout.write(f"{width} {height}\n")
+    fout.write(f"{colorNum}\n")
+    
+    for line in artFile:
+        xCoordinate = random.randrange(width)
+        yCoordinate = random.randrange(height)
+        redNum, greenNum, blueNum = myImage.getPixel(xCoordinate, yCoordinate)
+        radius = random.randrange(1, 10)
+        fout.write(f"{redNum} {greenNum} {blueNum} {xCoordinate} {yCoordinate} {radius}\n")
+
+    fout.close()
+
+    artFileSplit = artFile.split('/')
+    finalArtFile = artFileSplit[-1]
+    print(f"File created: {finalArtFile}")
+    print("Program ending.")
+
 
 main()
+
+#what didn't I do?
+#didn't write almost anything to file
+#didn't close the file 
+# didn't kept just the file part but the whole location
+
+
